67536f61a78ffd04e2ebd01e46ddc79c553381fa
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-09-22 18:54:37 +0300

    feat: add burger menu on 768px

--- museum/src/js/burger.js
+++ museum/src/js/burger.js
@@ -2,8 +2,10 @@ const burger = document.querySelector('.burger__menu');
 const headerNav = document.querySelector('.list');
 const welcomeText = document.querySelector('.welcome__info');
 
-burger.addEventListener('click', () => {
+const burgerMenuActive = () => {
   burger.classList.toggle('burger__close');
   headerNav.classList.toggle('list__active');
   welcomeText.classList.toggle('welcome__info__hidden');
-});
+};
+
+burger.addEventListener('click', burgerMenuActive);
