c5f487093ec445cfd3dd30e1883973919f334521
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-09-26 17:04:43 +0300

    fix: fix burger menu at 768px

--- museum/src/js/burger.js
+++ museum/src/js/burger.js
@@ -1,11 +1,24 @@
 const burger = document.querySelector('.burger__menu');
 const headerNav = document.querySelector('.list');
 const welcomeText = document.querySelector('.welcome__info');
+const navMobile = document.querySelector('.nav__mobile');
+
+const burgerMenuMobileActive = () => {
+  if (window.innerWidth <= 768 && headerNav.classList.contains('list__active')) {
+    document.body.style.overflow = 'hidden';
+    navMobile.classList.add('nav__mobile__active');
+  } else {
+    document.body.style.overflow = 'visible';
+    navMobile.classList.remove('nav__mobile__active');
+  }
+};
 
 const burgerMenuActive = () => {
   burger.classList.toggle('burger__close');
   headerNav.classList.toggle('list__active');
   welcomeText.classList.toggle('welcome__info__hidden');
+  burgerMenuMobileActive();
 };
 
 burger.addEventListener('click', burgerMenuActive);
+window.addEventListener('resize', burgerMenuMobileActive);
