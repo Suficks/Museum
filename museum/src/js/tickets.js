f08d1240199d8d7df18af981bea13c557245a934
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-09-29 15:22:36 +0300

    feat: add form validation

--- museum/src/js/tickets.js
+++ museum/src/js/tickets.js
@@ -1,3 +1,7 @@
+/* eslint-disable default-case */
+// eslint-disable-next-line import/no-extraneous-dependencies
+import IMask from 'imask';
+
 // Открытие/закрытие модального окна
 
 const buyBtn = document.querySelector('.buy');
@@ -43,3 +47,92 @@ const createRipple = (e) => {
 bookBtn.addEventListener('click', createRipple);
 
 // Ripple эффект
+
+// Валидация форм
+
+const bookingModal = document.querySelector('.booking__modal');
+const inputs = bookingModal.querySelectorAll('input');
+
+const MINMANELENGTH = 3;
+const MAXMANELENGTH = 15;
+const MAXPHONELENGTH = 16;
+const NAME_REGEXP = /[^a-zа-яё\s]/gi;
+const EMAIL_REGEXP = /^[a-zA-Z0-9_-]{3,15}@[a-zA-Z]{4,}\.[a-zA-Z]{2,}$/;
+
+const errorMessage = {
+  nameTooShort: 'Минимальное количество символов 3',
+  nameTooLong: 'Максимальное количество символов 15',
+  invalidSymbols: 'Введите имя, от 3 до 15 символов, используя только буквы и пробел',
+  invalidEmail: 'Пожалуйста, введите корректный email-адрес',
+  phoneTooLong: 'Максимальное количество цифр 10',
+};
+
+const validation = (item) => {
+  const error = item.nextElementSibling;
+  const inputName = item.getAttribute('data-input');
+
+  // let isCorrect = false;
+
+  switch (inputName) {
+    case ('name'): {
+      if (item.value === '') {
+        item.classList.remove('input__invalid');
+        error.innerHTML = '';
+      } else if (NAME_REGEXP.test(item.value)) {
+        item.classList.add('input__invalid');
+        error.innerHTML = errorMessage.invalidSymbols;
+      } else if (item.value.length < MINMANELENGTH) {
+        item.classList.add('input__invalid');
+        error.innerHTML = errorMessage.nameTooShort;
+      } else if (item.value.length > MAXMANELENGTH) {
+        item.classList.add('input__invalid');
+        error.innerHTML = errorMessage.nameTooLong;
+      } else {
+        item.classList.remove('input__invalid');
+        error.innerHTML = '';
+        // isCorrect = true;
+      }
+      break;
+    }
+
+    case ('email'): {
+      if (item.value === '') {
+        item.classList.remove('input__invalid');
+        error.innerHTML = '';
+      } else if (!EMAIL_REGEXP.test(item.value)) {
+        item.classList.add('input__invalid');
+        error.innerHTML = errorMessage.invalidEmail;
+      } else {
+        item.classList.remove('input__invalid');
+        error.innerHTML = '';
+        // isCorrect = true;
+      }
+      break;
+    }
+
+    case ('tel'): {
+      const maskOptions = {
+        mask: '+{7}(000)000-00-00',
+      };
+      const mask = IMask(item, maskOptions);
+      mask.updateValue();
+      if (item.value.length > MAXPHONELENGTH) {
+        item.classList.add('input__invalid');
+        error.innerHTML = errorMessage.phoneTooLong;
+      } else {
+        item.classList.remove('input__invalid');
+        error.innerHTML = '';
+        // isCorrect = true;
+      }
+      break;
+    }
+  }
+};
+
+inputs.forEach((item) => {
+  item.addEventListener('input', () => {
+    validation(item);
+  });
+});
+
+// Валидация форм
