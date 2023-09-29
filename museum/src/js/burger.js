3663d47c07aa2c5aeb978d4a2d641f8dc5d6ff89
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-09-29 11:12:18 +0300

    feat: add burger close on click outside it

--- museum/src/js/burger.js
+++ museum/src/js/burger.js
@@ -4,6 +4,8 @@ const welcomeText = document.querySelector('.welcome__info');
 const navMobile = document.querySelector('.nav__mobile');
 const burgerMenuItems = document.querySelectorAll('.burger__item__link');
 
+// Бургер меню на разрешении < 768px
+
 const burgerMenuMobileActive = () => {
   if (window.innerWidth <= 768 && headerNav.classList.contains('list__active')) {
     document.body.style.overflow = 'hidden';
@@ -14,6 +16,10 @@ const burgerMenuMobileActive = () => {
   }
 };
 
+// Бургер меню на разрешении < 768px
+
+// Открытие бургер меню
+
 const burgerMenuActive = () => {
   burger.classList.toggle('burger__close');
   headerNav.classList.toggle('list__active');
@@ -21,8 +27,30 @@ const burgerMenuActive = () => {
   burgerMenuMobileActive();
 };
 
+// Открытие бургер меню
+
+// Закрытие меню при клике по любому месту
+
+const closeMenuOnClickOutside = (event) => {
+  const { target } = event;
+
+  if (target !== burger) {
+    burger.classList.remove('burger__close');
+    headerNav.classList.remove('list__active');
+    welcomeText.classList.remove('welcome__info__hidden');
+    burgerMenuMobileActive();
+  }
+};
+
+// Закрытие меню при клике по любому месту
+
+// Вызов функций
+
 burger.addEventListener('click', burgerMenuActive);
 window.addEventListener('resize', burgerMenuMobileActive);
 burgerMenuItems.forEach((item) => {
   item.addEventListener('click', burgerMenuActive);
 });
+document.addEventListener('click', closeMenuOnClickOutside);
+
+// Вызов функций
