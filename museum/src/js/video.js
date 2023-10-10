e00362c4b136cd0592e7341fdbcfbeb7843d0f2f
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-10-10 16:49:20 +0300

    feat: add video controls functional

--- museum/src/js/video.js
+++ museum/src/js/video.js
@@ -6,24 +6,19 @@ const volumeBtn = document.querySelector('.volume');
 const volumeBar = document.querySelector('.volume__bar');
 const fullscreenBtn = document.querySelector('.fullscreen');
 const videoContainer = document.querySelector('.video__items__wrapper');
-const sliderBtns = document.querySelector('.slider__buttons');
+const sliderBtns = document.querySelectorAll('.slider__buttons');
 const videoControls = document.querySelector('.video__controls');
-const iframe = document.querySelector('.iframe');
+// const iframe = document.querySelector('.iframe');
 
 const backgroundNull = 'linear-gradient(to right, rgb(113, 7, 7) 0%, rgb(113, 7, 7) 0%, rgb(196, 196, 196) 0%, rgb(196, 196, 196) 100%)';
+
+let timer;
 
 const videoPlay = (video) => {
   video.play();
   bigPlay.style.opacity = '0';
   play.style.backgroundImage = 'url(assets/pause.svg)';
   videoControls.style.opacity = '0';
-
-  videoControls.addEventListener('mouseenter', () => {
-    videoControls.style.opacity = '1';
-  });
-  videoControls.addEventListener('mouseleave', () => {
-    videoControls.style.opacity = '0';
-  });
 };
 
 const videoPause = (video) => {
@@ -31,13 +26,16 @@ const videoPause = (video) => {
   bigPlay.style.opacity = '1';
   play.style.backgroundImage = 'url(assets/play.svg)';
   videoControls.style.opacity = '1';
+};
 
-  videoControls.addEventListener('mouseenter', () => {
-    videoControls.style.opacity = '1';
-  });
-  videoControls.addEventListener('mouseleave', () => {
-    videoControls.style.opacity = '1';
-  });
+const hideControls = (video) => {
+  console.log(video.play);
+  if (!video.paused) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      videoControls.style.opacity = '0';
+    }, 6000);
+  }
 };
 
 const toggleVideoStatus = (video) => {
@@ -58,6 +56,16 @@ const videoChangeTime = (video, e) => {
   const progress = e.offsetX / (progressLine.offsetWidth / 100);
   video.currentTime = video.duration * (progress / 100);
   progressLine.style.background = `linear-gradient(to right, rgb(113, 7, 7) 0%, rgb(113, 7, 7) ${progress}%, rgb(196, 196, 196) ${progress}%, rgb(196, 196, 196) 100%)`;
+};
+
+const mouseMoveHandler = (e) => {
+  videos.forEach((video) => {
+    videoChangeTime(video, e);
+  });
+};
+
+const mouseUpHandler = () => {
+  progressLine.removeEventListener('mousemove', mouseMoveHandler);
 };
 
 let savedVolume = 50;
@@ -93,10 +101,14 @@ const toggleFullscreen = (video) => {
     videoContainer.requestFullscreen()
       .catch((error) => console.log('Fullscreen request failed:', error));
     video.classList.add('video__item__fullscreen');
+    progressLine.classList.add('progress__line__fullscreen');
+    volumeBar.classList.add('volume__bar__fullscreen');
     fullscreenBtn.style.backgroundImage = 'url(assets/fullscreen_exit.svg)';
   } else if (document.fullscreenEnabled) {
     document.exitFullscreen();
     video.classList.remove('video__item__fullscreen');
+    progressLine.classList.remove('progress__line__fullscreen');
+    volumeBar.classList.remove('volume__bar__fullscreen');
     fullscreenBtn.style.backgroundImage = 'url(assets/fullscreen.svg)';
   }
 };
@@ -116,6 +128,7 @@ videos.forEach((video) => {
   video.addEventListener('timeupdate', () => {
     videoProgress(video);
   });
+  video.addEventListener('ended', videosStop);
   play.addEventListener('click', () => {
     toggleVideoStatus(video);
   });
@@ -128,27 +141,29 @@ videos.forEach((video) => {
   progressLine.addEventListener('click', (e) => {
     videoChangeTime(video, e);
   });
+  videoControls.addEventListener('mouseout', () => {
+    hideControls(video);
+  });
   volumeBar.addEventListener('change', () => {
+    changeVolume(video);
+  });
+  volumeBar.addEventListener('mousemove', () => {
     changeVolume(video);
   });
   fullscreenBtn.addEventListener('click', () => {
     toggleFullscreen(video);
   });
-
-  window.addEventListener('message', (e) => {
-    if (e.data === 'videoPause') {
-      videoPause(video);
-    }
-  });
 });
 
-// iframes.forEach((iframe) => {
-document.addEventListener('DOMContentLoaded', () => {
-  iframe.addEventListener('click', () => {
-    console.log(iframe);
-    window.parent.postMessage('videoPause', '*');
-  });
+progressLine.addEventListener('mousedown', () => {
+  progressLine.addEventListener('mousemove', mouseMoveHandler);
 });
-// });
+document.addEventListener('mouseup', mouseUpHandler);
 
-sliderBtns.addEventListener('click', videosStop);
+videoControls.addEventListener('mouseleave', () => {
+  videoControls.style.opacity = '1';
+});
+
+sliderBtns.forEach((item) => {
+  item.addEventListener('click', videosStop);
+});
